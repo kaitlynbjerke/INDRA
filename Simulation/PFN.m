314c1b983bac33362ac32185917c32356feda62e
rayringston <rringsto@stevens.edu>
2026-03-06 12:30:47 -0500

    Updated PFN.m
    Updated MATLAB script to include all necessary calculations

--- Simulation/PFN.m
+++ Simulation/PFN.m
@@ -1,6 +1,18 @@
-T = 200e-9; % pulse duration
-Z = 50; % load impedance
-N = 6; % number of stages
+% --------------- Characteristics of pulse
+T = 0.6e-3;          % pulse length in seconds
+ZLoad = 50;         % load impedance in ohms
 
-C0 = (Z * T) / (2 * N)
-L0 = T / (2 * Z * N)
+% --------------- Marx-PFN setup
+PFNStages = 6;      % number of stages in each PFN
+MarxStages = 1;     % number of marx stages, basically just number of PFNs
+
+% --------------- Calculated values
+ZStage = ZLoad / MarxStages;
+
+C0 = T / (2 * ZStage * PFNStages)   % C = T/(2ZN)
+L = (ZStage * T) / (2 * PFNStages)  % L = ZT/2N
+L0 = L/2;
+LTerm = L0 * 1.3                    % +30% inductance helps to prevent overshoot
+
+RCharge = 100e-9 / (C0 * PFNStages);
+LIso = 100 * L;
